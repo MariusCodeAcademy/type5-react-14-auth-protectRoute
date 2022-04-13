cae5f2a09ba815878db88ba8c5699a8a2585b144
Marius <marius@example.com>
2022-04-13 14:32:35 +0300

    form Handler

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,13 @@
 import Container from './../components/UI/Container';
 function LoginPage() {
+  function formHandler(e) {
+    e.preventDefault();
+    console.log('Logging in');
+  }
   return (
     <Container>
       <h1>Login</h1>
-      <form>
+      <form onSubmit={formHandler}>
         <input type='text' placeholder='email' />
         <br />
         <input type='text' placeholder='password' />
