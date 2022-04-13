ccc43e319c2cb554135931d2f9cd87e9661ffcbf
Marius <marius@example.com>
2022-04-13 13:52:14 +0300

    header html router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { Route } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <div className='App'>
-      <h1>App</h1>
+      <Header />
+      <Route path={'/'} exact>
+        <h1>Home</h1>
+      </Route>
+      <Route path={'/login'}>
+        <LoginPage />
+      </Route>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import { NavLink } from 'react-router-dom';
+
+function Header() {
+  return (
+    <header className='header'>
+      <h2>System</h2>
+      <nav>
+        <NavLink to={'/'}>Home</NavLink>
+        <NavLink to={'/vip'}>Vip</NavLink>
+        <NavLink to={'/login'}>Login</NavLink>
+        <NavLink to={'/logout'}>Logout</NavLink>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
